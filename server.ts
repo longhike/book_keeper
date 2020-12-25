e89b1ab0b9d7ef8de026dd219bcc638e563c51ba
longhike <sam.ascheim@gmail.com>
2020-12-24 20:12:15 -0500

    html successfully served

--- server.ts
+++ server.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import path from 'path'; // use this for dynamic path once compiled
 
 const app: express.Application = express();
 
-const PORT: any = process.env.PORT || 3030;
+const PORT: string | number = process.env.PORT || 3030;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.sendFile('index.html')
+})
 
 app.get('/ping', (req: express.Request, res: express.Response) => {
     res.send("Hello!")
